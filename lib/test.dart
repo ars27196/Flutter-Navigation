111dd66283e11dbf59acd3a2574a99b41a180241
ars27196 <ars27196@gmail.com>
2020-03-02 01:39:54 +0500

    dashboard design partially done

--- lib/test.dart
+++ lib/test.dart
@@ -14,6 +14,15 @@ class Test extends StatefulWidget {
 }
 
 class TestState extends State {
+  double dropdownWidgetWidthSize = 0;
+  GlobalKey _dropdownWidgetKey = GlobalKey<TestState>();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayoutLoaded);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     var thirtyPercent = (MediaQuery.of(context).size.height) * 0.3;
@@ -21,134 +30,192 @@ class TestState extends State {
     return Scaffold(
         body: SafeArea(
       child: Container(
-        child: Column(
+        child: ListView(
+          padding: EdgeInsets.only(bottom: 30),
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: thirtyPercent,
-              color: Colors.deepOrangeAccent,
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    height: 55,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.apps),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text("Adill Fayyaz",
-                                style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold,color: Colors.white)),
-                            Text("20 March 2020",
-                                style: TextStyle(
-                                    fontSize: 10, color: Colors.white)),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.replay),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 65, left: 20, right: 20),
-                    child: Container(
-                      width: double.infinity,
-                      height: 60,
-                      color: Colors.white,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text("Today")
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only( left: 20, right: 20),
-              width: double.infinity,
-              height: seventyPercent,
-              color: Colors.blueGrey,
-              child: Stack(
-                overflow: Overflow.visible,
-                children: <Widget>[
-                  Positioned(
-                    top: -50,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          width: 350,
-                          height: 200,
+            Column(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  height: thirtyPercent,
+                  color: Colors.deepOrangeAccent,
+                  child: Stack(
+                    children: <Widget>[
+                      _header(),
+                      Padding(
+                        padding: EdgeInsets.only(top: 65, left: 20, right: 20),
+                        child: Container(
+                          key: _dropdownWidgetKey,
+                          width: double.infinity,
+                          height: 60,
                           color: Colors.white,
-
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[Text("Today")],
+                          ),
                         ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-
-                  Padding(
-                    padding: EdgeInsets.only(top: 200, left: 20,right: 20),
-                    child: Container(
-                      child:GridView.count(crossAxisCount: 2
-                      ,
-                      children: <Widget>[
-                        _card(),
-                        _card(),
-                        _card(),
-                        _card()
-
-                      ],) ,
-                    ),
-                  )
-                ],
-              ),
-            )
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: seventyPercent,
+                  color: Colors.blueGrey,
+                  child: Stack(
+                    overflow: Overflow.visible,
+                    children: <Widget>[
+                      Positioned(
+                        top: -50,
+                        child: Container(
+                          width: dropdownWidgetWidthSize,
+                          height: 200,
+                          decoration: _decorationCricularRect(),
+                        ),
+                      ),
+                      //bottom cards
+                      Padding(
+                        padding: EdgeInsets.only(top: 180),
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      child: _card("Revenue Generated","Rs 12,000"),
+                                      padding: EdgeInsets.only(left: 0, right: 10),
+                                    ),
+                                    Padding(
+                                      child: _card("Expense Claim","Rs 0"),
+                                      padding: EdgeInsets.only(left: 10, right: 00),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      child: _card("Manual Order","200"),
+                                      padding: EdgeInsets.only(top: 20, right: 10),
+                                    ),
+                                    Padding(
+                                      child: _card("Messages","2"),
+                                      padding: EdgeInsets.only(top: 20, left: 10),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
     ));
   }
 
-  _card() {
+  _card(String title,String price) {
     return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(4.0),
+      decoration: _decorationCricularRect(),
+      margin: EdgeInsets.only(right: 4.0),
+      padding: EdgeInsets.only(left: 15, right: 15, top: 5,bottom: 5),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.only(top:20.0,left: 12, right: 12),
+            padding: const EdgeInsets.only(
+                top: 20.0, left: 10, right: 10, bottom: 5),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(width: 80,child: Text("Revenue Genrated",maxLines:2)),
-                Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: Icon(Icons.access_alarm,size: 30,),
-                )]),
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      width: 80,
+                      child: Text(
+                        title,
+                        maxLines: 2,
+                        style: TextStyle(fontSize: 16),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Icon(
+                      Icons.access_alarm,
+                      size: 30,
+                    ),
+                  )
+                ]),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top:10.0,left: 12, right: 12),
-                child: Text("Rs 12000", style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.only( top: 10.0, left: 10, right: 10, bottom: 5),
+            child: Row(
+              children: <Widget>[
+                Text(price,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
+              ],
+            ),
           )
         ],
       ),
-
     );
   }
+
+  _header() {
+    return Container(
+      height: 55,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.apps),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Adill Fayyaz",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              Text("20 March 2020",
+                  style: TextStyle(fontSize: 10, color: Colors.white)),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.replay),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _afterLayoutLoaded(_) {
+    setState(() {
+      dropdownWidgetWidthSize = _getWidthSize();
+    });
+  }
+
+  double _getWidthSize() {
+    final RenderBox renderBoxRed =
+        _dropdownWidgetKey.currentContext.findRenderObject();
+    return renderBoxRed.size.width;
+  }
+
+  _decorationCricularRect()=>
+      new BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius:BorderRadius.all(Radius.circular(10)) ,
+        border: Border.all(width: 5.0, color: Colors.white),
+      );
 }
 
 _buttonParentClass() {
